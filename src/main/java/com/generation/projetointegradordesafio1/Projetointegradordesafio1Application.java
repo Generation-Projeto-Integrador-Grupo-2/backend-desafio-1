79f6e397ee98d8ad69fadc8d7d49a2d5e0e79c65
Laura Nery <laura.olivernery@gmail.com>
2025-04-17 09:51:04 -0300

    estrutura inicial

---
+++ src/main/java/com/generation/projetointegradordesafio1/Projetointegradordesafio1Application.java
@@ -0,0 +1,13 @@
+package com.generation.projetointegradordesafio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetointegradordesafio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetointegradordesafio1Application.class, args);
+	}
+
+}
